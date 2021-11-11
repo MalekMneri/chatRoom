40d00a0248dc2547d267712bce5e0ebd1649d0b5
Malek <mnerimalek@gmail.com>
2021-11-11 16:07:05 +0100

    test

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const port = process.env.PORT || 6969;
 const server = http.createServer(express);
 const wsServer = new websocket.Server({ server });
 
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "index.html");
+});
+
 wsServer.on("connection", function connection(ws) {
   ws.on("message", function incoming(message) {
     wsServer.clients.forEach(function each(client) {
