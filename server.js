4db88b42b1140a4ab361d7e4e70fa6f700ebcc60
Malek <mnerimalek@gmail.com>
2021-11-11 15:48:02 +0100

    initial Commit!

---
+++ server.js
@@ -0,0 +1,21 @@
+const websocket = require("ws");
+const http = require("http");
+const express = require("express");
+
+const port = process.env.PORT || 6969;
+const server = http.createServer(express);
+const wsServer = new websocket.Server({ server });
+
+wsServer.on("connection", function connection(ws) {
+  ws.on("message", function incoming(message) {
+    wsServer.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === websocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
+
+server.listen(port, function listening() {
+  console.log(`Server listening on ${port}`);
+});
